530493eabcda93e519ac5f4ae41af2c0b0a9ad5a
Haraldur-Dadi <halli.heima@gmail.com>
2020-03-30 04:13:55 +0000

    Fixed bug with unable to close menu

--- Assets/CrossSceneManager.cs
+++ Assets/CrossSceneManager.cs
@@ -7,6 +7,8 @@ public class CrossSceneManager : MonoBehaviour {
     public static CrossSceneManager Instance;
 
     public GameObject settingsUI;
+    public Button settingsBtn;
+    public Button settingsCloseBtn;
 
     private void Awake() {
         if (Instance == null) {
@@ -22,8 +24,11 @@ public class CrossSceneManager : MonoBehaviour {
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
         settingsUI = GameObject.Find("SettingsUI");
 
-        Button settingsBtn = GameObject.Find("SettingsBtn").GetComponent<Button>();
+        settingsBtn = GameObject.Find("SettingsBtn").GetComponent<Button>();
+        settingsCloseBtn = settingsUI.GetComponentInChildren<Button>();
+
         settingsBtn.onClick.AddListener(delegate { ToggleSettings(); });
+        settingsCloseBtn.onClick.AddListener(delegate { ToggleSettings(); });
     }
 
     public void ToggleSettings() {
