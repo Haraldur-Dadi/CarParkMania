e4cc040210b38a4038392fd4cf597c0a048b941e
Haraldur-Dadi <halli.heima@gmail.com>
2020-04-21 22:47:35 +0000

    Update Level Buttons

--- Assets/LevelButton.cs
+++ Assets/LevelButton.cs
@@ -6,5 +6,26 @@ public class LevelButton : MonoBehaviour {
 
     public Button button;
     public TextMeshProUGUI text;
+    public GameObject checkMark;
     public GameObject locked;
+
+    public void Unavailable() {
+        text.gameObject.SetActive(false);
+        checkMark.SetActive(false);
+    }
+
+    public void Finished(int level) {
+        locked.SetActive(false);
+        BtnStandard(level);
+    }
+
+    public void NextLevel(int level) {
+        locked.SetActive(false);
+        checkMark.SetActive(false);
+        BtnStandard(level);
+    }
+
+    private void BtnStandard(int level) {
+        text.text = level.ToString();
+    }
 }
