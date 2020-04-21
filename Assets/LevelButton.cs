47a1d3d138d77d618d9b61f15fede701a6718e88
Haraldur-Dadi <halli.heima@gmail.com>
2020-04-21 22:34:00 +0000

    Made a level button prefab and script

---
+++ Assets/LevelButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelButton : MonoBehaviour {
+
+    public Button button;
+    public TextMeshProUGUI text;
+    public GameObject locked;
+}
