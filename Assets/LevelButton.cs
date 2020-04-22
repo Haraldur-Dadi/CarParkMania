754dcd5727a20c2a5773d54e61d67c41147b5787
Haraldur-Dadi <halli.heima@gmail.com>
2020-04-22 00:42:01 +0000

    Updated LevelButtons

--- Assets/LevelButton.cs
+++ Assets/LevelButton.cs
@@ -9,23 +9,24 @@ public class LevelButton : MonoBehaviour {
     public GameObject checkMark;
     public GameObject locked;
 
-    public void Unavailable() {
-        text.gameObject.SetActive(false);
+    public void Unavailable(int level) {
+        button.interactable = false;
+        locked.SetActive(true);
         checkMark.SetActive(false);
+        text.text = level.ToString();
     }
 
     public void Finished(int level) {
+        button.interactable = true;
         locked.SetActive(false);
-        BtnStandard(level);
+        checkMark.SetActive(true);
+        text.text = level.ToString();
     }
 
     public void NextLevel(int level) {
+        button.interactable = true;
         locked.SetActive(false);
         checkMark.SetActive(false);
-        BtnStandard(level);
-    }
-
-    private void BtnStandard(int level) {
         text.text = level.ToString();
     }
 }
