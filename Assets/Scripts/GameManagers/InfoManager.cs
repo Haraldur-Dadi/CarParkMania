36e31b248bb1d421bfd980cb0d4ccb2ce00b6021
Haraldur-Dadi <halli.heima@gmail.com>
2020-04-23 22:12:45 +0000

    Update InfoManager

--- Assets/Scripts/GameManagers/InfoManager.cs
+++ Assets/Scripts/GameManagers/InfoManager.cs
@@ -9,19 +9,15 @@ public class InfoManager : MonoBehaviour {
     public TextMeshProUGUI totalCompleted;
     public TextMeshProUGUI[] casualCompleted;
     public TextMeshProUGUI[] challengeCompleted;
-    public TextMeshProUGUI[] timedCompleted;
 
     private void Start() {
         int casualReached = PlayerPrefs.GetInt("CasualLevelReached", 0);
         int challengeReached = PlayerPrefs.GetInt("ChallengeLevelReached", 0);
-        int timedReached = PlayerPrefs.GetInt("TimedLevelReached", 0);
 
-        totalCompleted.text = "Total: " + (casualReached + challengeReached + timedReached) + "/" + standardTotalLevels*3;
+        totalCompleted.text = "Total: " + (casualReached + challengeReached) + "/" + standardTotalLevels*2;
         casualCompleted[0].text = ((float) casualReached / standardTotalLevels) * 100 + "%";
         casualCompleted[1].text = casualReached + "/" + standardTotalLevels;
         challengeCompleted[0].text = ((float) challengeReached / standardTotalLevels) * 100 + "%";
         challengeCompleted[1].text = challengeReached + "/" + standardTotalLevels;
-        timedCompleted[0].text = ((float) timedReached / standardTotalLevels) * 100 + "%";
-        timedCompleted[1].text = timedReached + "/" + standardTotalLevels;
     }
 }
