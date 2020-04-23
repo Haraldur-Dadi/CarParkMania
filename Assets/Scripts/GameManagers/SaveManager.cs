5f8ff88e89818b76b4d4e15df21feccdb0ab13a0
Haraldur-Dadi <halli.heima@gmail.com>
2020-04-23 14:45:51 +0000

    Update SaveManager
    Remove unused lines

--- Assets/Scripts/GameManagers/SaveManager.cs
+++ Assets/Scripts/GameManagers/SaveManager.cs
@@ -25,17 +25,14 @@ public class SaveManager : MonoBehaviour {
 
     public void SaveIntData(string varName, int value) {
         PlayerPrefs.SetInt(varName, value);
-        //PlayerPrefs.Save();
     }
 
     public void SaveFloatData(string varName, float value) {
         PlayerPrefs.SetFloat(varName, value);
-        //PlayerPrefs.Save();
     }
 
     public void SaveStringData(string varName, string value) {
         PlayerPrefs.SetString(varName, value);
-        //PlayerPrefs.Save();
     }
 
     public void IncreaseAchivementProgress(int achivementID) {
