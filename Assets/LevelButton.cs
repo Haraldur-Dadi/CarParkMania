5af67ac6b6448e8f5c9b0193df6881bab5fb7118
Haraldur-Dadi <halli.heima@gmail.com>
2020-04-23 13:54:21 +0000

    Update LevelButton
    Now shows right level

--- Assets/LevelButton.cs
+++ Assets/LevelButton.cs
@@ -13,20 +13,20 @@ public class LevelButton : MonoBehaviour {
         button.interactable = false;
         locked.SetActive(true);
         checkMark.SetActive(false);
-        text.text = level.ToString();
+        text.text = (level + 1).ToString();
     }
 
     public void Finished(int level) {
         button.interactable = true;
         locked.SetActive(false);
         checkMark.SetActive(true);
-        text.text = level.ToString();
+        text.text = (level + 1).ToString();
     }
 
     public void NextLevel(int level) {
         button.interactable = true;
         locked.SetActive(false);
         checkMark.SetActive(false);
-        text.text = level.ToString();
+        text.text = (level + 1).ToString();
     }
 }
