65c1fabb616c2b231dfdb636771d85b84e28db88
Haraldur-Dadi <halli.heima@gmail.com>
2020-04-25 15:50:01 +0000

    Updated Challenge Levels and db
    Created new levels and removed information for 2 stars and 3 stars, now only contains minimum moves to complete

--- Assets/Scripts/ChallengeLevels/ChallengeLevel.cs
+++ Assets/Scripts/ChallengeLevels/ChallengeLevel.cs
@@ -1,20 +1,14 @@
 ﻿public class ChallengeLevel {
     public int ID;
-    public int star1;
-    public int star2;
-    public int star3;
+    public int minMoves;
 
-    public ChallengeLevel (int ID, int star1, int star2, int star3) {
+    public ChallengeLevel (int ID, int minMoves) {
         this.ID = ID;
-        this.star1 = star1;
-        this.star2 = star2;
-        this.star3 = star3;
+        this.minMoves = minMoves;
     }
 
     public ChallengeLevel (ChallengeLevel level) {
         this.ID = level.ID;
-        this.star1 = level.star1;
-        this.star2 = level.star2;
-        this.star3 = level.star3;
+        this.minMoves = level.minMoves;
     }
 }
--- Assets/Scripts/ChallengeLevels/ChallengeLevelDb.cs
+++ Assets/Scripts/ChallengeLevels/ChallengeLevelDb.cs
@@ -15,7 +15,31 @@ public class ChallengeLevelDb : MonoBehaviour {
 
     void BuildLevelDb() {
         ChallengeLevels = new List<ChallengeLevel> () {
-            new ChallengeLevel(0, 5, 7, 10)
+            new ChallengeLevel(0, 6),
+            new ChallengeLevel(1, 5),
+            new ChallengeLevel(2, 7),
+            new ChallengeLevel(3, 7),
+            new ChallengeLevel(4, 5),
+            new ChallengeLevel(5, 5),
+            new ChallengeLevel(6, 6),
+            new ChallengeLevel(7, 7),
+            new ChallengeLevel(8, 5),
+            new ChallengeLevel(9, 7),
+            new ChallengeLevel(10, 7),
+            new ChallengeLevel(11, 6),
+            new ChallengeLevel(12, 5),
+            new ChallengeLevel(13, 6),
+            new ChallengeLevel(14, 8),
+            new ChallengeLevel(15, 7),
+            new ChallengeLevel(16, 8),
+            new ChallengeLevel(17, 6),
+            new ChallengeLevel(18, 8),
+            new ChallengeLevel(19, 8),
+            new ChallengeLevel(20, 8),
+            new ChallengeLevel(21, 8),
+            new ChallengeLevel(22, 8),
+            new ChallengeLevel(23, 8),
+            new ChallengeLevel(24, 8)
         };
     }
 }
