f41436c42585a7b1791bca08b9cf75977eb3984f
Haraldur-Dadi <halli.heima@gmail.com>
2020-04-25 00:37:28 +0000

    Created ChallengeLevel and ChallengeLevelDb
    Created a standard script for challenge levels and a db to store them

---
+++ Assets/Scripts/ChallengeLevels/ChallengeLevelDb.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeLevelDb : MonoBehaviour {
+    
+    public List<ChallengeLevel> ChallengeLevels;
+
+    private void Start() {
+        BuildLevelDb();
+    }
+
+    public ChallengeLevel GetChallengeLevel(int ID) {
+        return ChallengeLevels[ID];
+    }
+
+    void BuildLevelDb() {
+        ChallengeLevels = new List<ChallengeLevel> () {
+            new ChallengeLevel(0, 5, 7, 10)
+        };
+    }
+}
