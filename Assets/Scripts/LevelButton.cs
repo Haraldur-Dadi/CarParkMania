dcc7ce88247814ec44e43ccbc3f1446a7f9d084b
Haraldur-Dadi <halli.heima@gmail.com>
2020-04-27 02:45:47 +0000

    Update LevelButton
    Added support for challengeLevelBtn

--- Assets/Scripts/LevelButton.cs
+++ Assets/Scripts/LevelButton.cs
@@ -8,25 +8,29 @@ public class LevelButton : MonoBehaviour {
     public TextMeshProUGUI text;
     public GameObject checkMark;
     public GameObject locked;
+    public Image star;
 
     public void Unavailable(int level) {
         button.interactable = false;
         locked.SetActive(true);
-        checkMark.SetActive(false);
+        if (checkMark)
+            checkMark.SetActive(false);
         text.text = (level + 1).ToString();
     }
 
     public void Finished(int level) {
         button.interactable = true;
         locked.SetActive(false);
-        checkMark.SetActive(true);
+        if (checkMark)
+            checkMark.SetActive(true);
         text.text = (level + 1).ToString();
     }
 
     public void NextLevel(int level) {
         button.interactable = true;
         locked.SetActive(false);
-        checkMark.SetActive(false);
+        if (checkMark)
+            checkMark.SetActive(false);
         text.text = (level + 1).ToString();
     }
 }
