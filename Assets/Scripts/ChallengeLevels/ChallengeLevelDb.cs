68baf0145134f747ffee32c52cf452b1a6b36cd3
Haraldur-Dadi <halli.heima@gmail.com>
2020-04-30 00:12:55 +0000

    Update ChallengeLevelDb
    Fixed bug where could not get gold star in level 6

--- Assets/Scripts/ChallengeLevels/ChallengeLevelDb.cs
+++ Assets/Scripts/ChallengeLevels/ChallengeLevelDb.cs
@@ -20,7 +20,7 @@ public class ChallengeLevelDb : MonoBehaviour {
             new ChallengeLevel(2, 7),
             new ChallengeLevel(3, 7),
             new ChallengeLevel(4, 5),
-            new ChallengeLevel(5, 5),
+            new ChallengeLevel(5, 6),
             new ChallengeLevel(6, 6),
             new ChallengeLevel(7, 7),
             new ChallengeLevel(8, 5),
