65902067421596a3f7bfb454a71a6b3f442f11b7
Halli <Halli.heima@gmail.com>
2020-12-29 15:02:31 +0000

    Update PageSwiper

--- Assets/Scripts/Menu/PageSwiper.cs
+++ Assets/Scripts/Menu/PageSwiper.cs
@@ -4,51 +4,23 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler {
-
-    private Vector2 startPos;
-    private Vector2 panelPos;
-    private RectTransform rectTransform;
-    public float swipeThreshold;
-
     public Shop shop;
     public About about;
-    public bool showNext;
+    CanvasGroup canvasGroup;
+    RectTransform rectTransform;
+    Vector2 startPos;
+    Vector2 panelPos;
+    float swipeThreshold;
 
-    public CanvasGroup canvasGroup;
-
-    private void Start() {
-        rectTransform = GetComponent<RectTransform>();
+    void Start() {
         canvasGroup = GetComponent<CanvasGroup>();
-        startPos = rectTransform.position;
-        panelPos = startPos;
-        if (shop) {
-            swipeThreshold = 0.1f;
-        } else {
-            swipeThreshold = 0.25f;
-        }
+        rectTransform = GetComponent<RectTransform>();
+        startPos = panelPos = rectTransform.position;
+        swipeThreshold = shop ? 0.1f : 0.25f;
     }
 
     public void OnDrag(PointerEventData data) {
         float differenceX = data.pressPosition.x - data.position.x;
-
-        if (differenceX > 0) {
-            if (shop) {
-                if (!shop.canShowNext)
-                    return;
-            } else if (about) {
-                if (!about.nextButton.activeSelf)
-                    return;
-            }
-        } else if (differenceX < 0) {
-            if (shop) {
-                if (!shop.canShowPrev)
-                    return;
-            } else if (about) {
-                if (!about.prevButton.activeSelf)
-                    return;
-            }
-        }
-        
         rectTransform.position = panelPos - new Vector2(differenceX, 0);
 
         if ((Mathf.Abs(differenceX) / Screen.width) >= swipeThreshold)
@@ -58,68 +30,48 @@ public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler {
     public void OnEndDrag(PointerEventData data) {
         Vector2 newPos = panelPos;
         float xThreshold = (data.pressPosition.x - data.position.x) / Screen.width;
-
-        if (Mathf.Abs(xThreshold) >= swipeThreshold) {
-            if (xThreshold > 0) {
-                if (shop) {
-                    if (!shop.canShowNext)
-                        return;
-                } else if (about) {
-                    if (!about.nextButton.activeSelf)
-                        return;
-                }
-
+        if (xThreshold >= swipeThreshold) {
+            if (shop && shop.canShowNext || about && about.nextButton.activeSelf) {
                 newPos += new Vector2(-Screen.width, 0);
-                showNext = true;
-            } else if (xThreshold < 0) {
-                if (shop) {
-                    if (!shop.canShowPrev)
-                        return;
-                } else if (about) {
-                    if (!about.prevButton.activeSelf)
-                        return;
-                }
-                
-                newPos += new Vector2(Screen.width, 0);
-                showNext = false;
+                StartCoroutine(ShowAnotherItem(true));
             }
-            StartCoroutine(ShowAnotherItem(newPos));
-            StartCoroutine(SmoothSwipe(newPos));
-        } else {
-            canvasGroup.alpha = 1;
-            StartCoroutine(SmoothSwipe(newPos));
+        } else if (xThreshold <= -swipeThreshold) {
+            if (shop && shop.canShowPrev || about && about.prevButton.activeSelf) {
+                newPos += new Vector2(Screen.width, 0);
+                StartCoroutine(ShowAnotherItem(false));
+            }
         }
+        StartCoroutine(SmoothSwipe(newPos));
     }
 
     IEnumerator SmoothSwipe(Vector2 endPos) {
         float t = 0f;
-        while(t <= 1) {
+        while (t <= 1) {
             t += Time.deltaTime / .4f;
             rectTransform.position = Vector2.Lerp(rectTransform.position, endPos, Mathf.SmoothStep(0f, 1f, t));
+            canvasGroup.alpha = t;
             yield return null;
         }
     }
 
-    IEnumerator ShowAnotherItem(Vector2 newPos) {
+    IEnumerator ShowAnotherItem(bool showNext) {
         yield return new WaitForSeconds(.4f);
-        canvasGroup.alpha = 1;
 
         if (showNext) {
             if (shop) {
                 shop.ShowNextItem(false);
-            } else if (about) {
+            } else {
                 about.ShowNextTutImage();
             }
-            rectTransform.position = panelPos + new Vector2(Screen.width * 2, 0);
         } else {
             if (shop) {
                 shop.ShowPrevItem(false);
-            } else if (about) {
+            } else {
                 about.ShowPrevTutImage();
             }
-            rectTransform.position = panelPos + new Vector2(-Screen.width * 2, 0);
         }
 
+        rectTransform.position = showNext ? panelPos + new Vector2(Screen.width * 2, 0) : panelPos + new Vector2(-Screen.width * 2, 0);
         StartCoroutine(SmoothSwipe(startPos));
     }
 }
