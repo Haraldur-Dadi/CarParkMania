cbc0a1487c2d7ceedbe37a0a1b1a36fecd75041d
Halli <Halli.heima@gmail.com>
2020-12-29 17:49:50 +0000

    Update about script

--- Assets/Scripts/Menu/About.cs
+++ Assets/Scripts/Menu/About.cs
@@ -1,58 +1,39 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class About : MonoBehaviour {
-
-    public int tutImgIndex;
+    public int tutImgIndex = 0;
     public GameObject[] tutorialImages;
 
     public GameObject prevButton;
     public GameObject nextButton;
-
     public TextMeshProUGUI indexTxt;
 
-    public void ResetTutImage() {
-        tutImgIndex = 0;
-
+    void Start() {
         for (int i = 0; i < tutorialImages.Length; i++) {
             tutorialImages[i].SetActive(false);
         }
-
         tutorialImages[tutImgIndex].SetActive(true);
         ToggleButtons();
     }
 
     public void ShowNextTutImage() {
         tutorialImages[tutImgIndex].SetActive(false);
-
-        tutImgIndex += 1;
+        tutImgIndex++;
         tutorialImages[tutImgIndex].SetActive(true);
-
         ToggleButtons();
     }
 
     public void ShowPrevTutImage() {
         tutorialImages[tutImgIndex].SetActive(false);
-
-        tutImgIndex -= 1;
+        tutImgIndex--;
         tutorialImages[tutImgIndex].SetActive(true);
-
         ToggleButtons();
     }
 
     public void ToggleButtons() {
         indexTxt.text = (tutImgIndex + 1) + "/" + tutorialImages.Length;
-
-        if (tutImgIndex == 0) {
-            prevButton.SetActive(false);
-            nextButton.SetActive(true);
-        } else if (tutImgIndex == tutorialImages.Length - 1) {
-            prevButton.SetActive(true);
-            nextButton.SetActive(false);
-        } else {
-            prevButton.SetActive(true);
-            nextButton.SetActive(true);
-        }
+        prevButton.SetActive(tutImgIndex != 0);
+        nextButton.SetActive(tutImgIndex != tutorialImages.Length - 1);
     }
 }
