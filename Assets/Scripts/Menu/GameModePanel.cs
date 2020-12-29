01bd0a7debe0f360e0a05b4282627f8aace3b34c
Halli <Halli.heima@gmail.com>
2020-12-29 20:54:36 +0000

    Update gamemodepanel

--- Assets/Scripts/Menu/GameModePanel.cs
+++ Assets/Scripts/Menu/GameModePanel.cs
@@ -2,23 +2,21 @@
 using UnityEngine.UI;
 
 public class GameModePanel : MonoBehaviour {
-    public string gameModeName;
     public Scrollbar scrollbar;
     public LevelButton[] levelBtns;
     public Button[] difficultyBtns;
-    private int levelReached;
 
-    public Sprite blankStar;
-    public Sprite bronzeStar;
-    public Sprite silverStar;
-    public Sprite goldStar;
-
-    private void Start() {
+    public void Hide() {
+        gameObject.SetActive(false);
+    }
+    public void Display() {
+        gameObject.SetActive(true);
         SelectDifficulty(CrossSceneManager.Instance.difficulty);
     }
 
     public void SelectDifficulty(int difficulty) {
-        levelReached = PlayerPrefs.GetInt(gameModeName + "LevelReached", 0);
+        string gameModeName = (CrossSceneManager.Instance.gameModeNr == 1) ? "Casual" : "Challenge";
+        int levelReached = PlayerPrefs.GetInt(gameModeName + "LevelReached", 0);
         CrossSceneManager.Instance.difficulty = difficulty;
         scrollbar.value = 1;
         
@@ -27,33 +25,18 @@ public class GameModePanel : MonoBehaviour {
             
             if (iValue > levelReached) {
                 levelBtns[i].Unavailable(iValue);
-                if (gameModeName == "Challenge") {
-                    levelBtns[i].star.enabled = false;
-                }
             } else {
-                levelBtns[i].button.onClick.AddListener(() => SelectLevel(CrossSceneManager.Instance.gameModeNr + 1, iValue));
-                if (gameModeName == "Casual") {
-                    if (iValue < levelReached) {
-                        levelBtns[i].Finished(iValue);
-                    } else {
-                        levelBtns[i].NextLevel(iValue);
-                    }
+                levelBtns[i].button.onClick.AddListener(() => SelectLevel(iValue));
+                if (gameModeName == "Casual" && iValue < levelReached) {
+                    levelBtns[i].Finished(iValue);
                 } else {
                     levelBtns[i].NextLevel(iValue);
-                    levelBtns[i].star.enabled = true;
-                    int stars = PlayerPrefs.GetInt("Challenge" + iValue + "Stars", 0);
-                    if (stars == 0) {
-                        levelBtns[i].star.sprite = blankStar;
-                    } else if (stars == 1) {
-                        levelBtns[i].star.sprite = bronzeStar;
-                    } else if (stars == 2) {
-                        levelBtns[i].star.sprite = silverStar;
-                    } else {
-                        levelBtns[i].star.sprite = goldStar;
-                    }
                 }
-
+                if (gameModeName == "Challenge") {
+                    levelBtns[i].SetStar(PlayerPrefs.GetInt("Challenge" + iValue + "Stars", 0));
+                }
             }
+            levelBtns[i].levelTxt.text = (iValue + 1).ToString();
         }
 
         for (int i = 0; i < difficultyBtns.Length; i++) {
@@ -65,9 +48,10 @@ public class GameModePanel : MonoBehaviour {
         }
     }
 
-    public void SelectLevel(int buildIndex, int level) {
+    public void SelectLevel(int level) {
+        AudioManager.Instance.PlayButtonClick();
         CrossSceneManager.Instance.TmpPreventClicks();
         SaveManager.Instance.SaveIntData("boardToLoad", level);
-        SceneFader.Instance.FadeToBuildIndex(buildIndex);
+        SceneFader.Instance.FadeToBuildIndex(CrossSceneManager.Instance.gameModeNr + 1);
     }
 }
