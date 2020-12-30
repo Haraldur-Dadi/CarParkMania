178b1d9d492023e349f4f6c4472cdc2ce79a7f06
Halli <Halli.heima@gmail.com>
2020-12-30 19:40:38 +0000

    tweaks made to car movement script

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -3,23 +3,15 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CarMovement : MonoBehaviour {
-
-    public GameManager gameManager;
-
     public Vector2 startPosOfCar; // Vector positio, used for undo
     public Vector2 posOfCar; // Vector position, used to determine current position of car
     public Vector2 posToMoveCar; // Vector position, used to determine where to move the car
 
     public Car carToMove;
-    public float snapValue;
+    float snapValue = 1.1f;
 
     public Button undoBtn;
-
     public List<UndoTuple> undoList;
-
-    private void Start() {
-        snapValue = 1.1f;
-    }
 
     public void Refresh() {
         undoList = new List<UndoTuple>();
@@ -32,27 +24,16 @@ public class CarMovement : MonoBehaviour {
         Vector2 raycastPos = posToMoveCar;
 
         if (carToMove.transform.eulerAngles.z == 0 || carToMove.transform.eulerAngles.z == 180) {
-            if (posToMoveCar.y > carToMove.transform.position.y) {
-                raycastPos.y += snapValue;
-            } else {
-                raycastPos.y -= snapValue;
-            }
+            raycastPos.y += (posToMoveCar.y > carToMove.transform.position.y) ? snapValue : -snapValue;
         } else {
-            if (posToMoveCar.x > carToMove.transform.position.x) {
-                raycastPos.x += snapValue;
-            } else {
-                raycastPos.x -= snapValue;
-            }
+            raycastPos.x += (posToMoveCar.x > carToMove.transform.position.x) ? snapValue : -snapValue;
         }
 
         RaycastHit2D[] hits = Physics2D.LinecastAll(raycastPos, carToMove.transform.position);
-
-        if (hits.Length > 0) {
-            foreach (RaycastHit2D hit in hits) {
-                Car car = hit.transform.GetComponent<Car>();
-                if (car != carToMove && car != null) {
-                    carInWay = true;
-                }
+        foreach (RaycastHit2D hit in hits) {
+            Car car = hit.transform.GetComponent<Car>();
+            if (car != carToMove && car != null) {
+                carInWay = true;
             }
         }
         return carInWay;
@@ -61,60 +42,46 @@ public class CarMovement : MonoBehaviour {
     Car RaycastAtPosition(Vector2 posToCheck) {
         /* Raycast at position to see if we hit Car object */
         RaycastHit2D hit = Physics2D.Raycast(posToCheck, Vector2.zero);
-
-        if (hit) {
-            return hit.transform.GetComponent<Car>();
-        }
-        return null;
+        return hit.transform.GetComponent<Car>();
     }
 
     private void Update() {
-        if (!gameManager.finished) {
-            /* If touch on screen determine what to do */
-            if (Input.touchCount > 0) {
-                Touch touch = Input.GetTouch(0);
+        /* If touch on screen determine what to do */
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
 
-                switch (touch.phase) {
-                    case TouchPhase.Began:
-                        // Get the screen position of the touch
-                        Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-                        carToMove = RaycastAtPosition(touchPos);
-
-                        // If the player touched a car, set variable carToMove and get position of Car
-                        if (carToMove) {
-                            startPosOfCar = carToMove.transform.position;
-                            posOfCar = startPosOfCar;
+            switch (touch.phase) {
+                case TouchPhase.Began:
+                    // Check if player touched a car
+                    carToMove = RaycastAtPosition(Camera.main.ScreenToWorldPoint(touch.position));
+                    if (carToMove) { startPosOfCar = posOfCar = carToMove.transform.position; }
+                    break;
+                case TouchPhase.Moved:
+                    // If we have a carToMove, determine where to move it on board
+                    if (carToMove) {
+                        // Get position for where to move the car
+                        posToMoveCar = SnapPosToGrid(Camera.main.ScreenToWorldPoint(touch.position), carToMove.maxValAxis);
+                        // If the snapPos of touchPos is not current position of car check if car in the way
+                        if (posToMoveCar != posOfCar && !CarInWay()) {
+                            carToMove.transform.position = posToMoveCar;
+                            posOfCar = posToMoveCar;
                         }
-                        break;
-                    case TouchPhase.Moved:
-                        // If we have a carToMove, determine where to move it on board
-                        if (carToMove) {
-                            // Get position for where to move the car
-                            Vector2 touchPosMoved = Camera.main.ScreenToWorldPoint(touch.position);
-                            touchPosMoved = SnapPosToGrid(touchPosMoved, carToMove.maxValAxis);
-                            // If the snapPos of touchPos is not current position of car check if car in the way
-                            if (touchPosMoved != posOfCar) {
-                                posToMoveCar = touchPosMoved;
-                                MoveCar();
-                            }
-                        }
-                        break;
-                    case TouchPhase.Canceled:
-                        EndMove();
-                        break;
-                    case TouchPhase.Ended:
-                        EndMove();
-                        break;
-                }
+                    }
+                    break;
+                case TouchPhase.Canceled:
+                    EndMove();
+                    break;
+                case TouchPhase.Ended:
+                    EndMove();
+                    break;
             }
         }
     }
 
     public Vector2 SnapPosToGrid(Vector2 snapPos, float maxValAxis) {
         /* Create grid like movement on board by rounding and clamping to within the boards border */
-
-        // Car is facing up/down and is moving on y-axis
         if (carToMove.transform.eulerAngles.z == 0 || carToMove.transform.eulerAngles.z == 180) {
+            // Car is facing up/down and is moving on y-axis
             snapPos.x = carToMove.transform.position.x;
             // Check if the position is outside of the maximum allowed movement values of car
             if (snapPos.y > maxValAxis) {
@@ -128,9 +95,8 @@ public class CarMovement : MonoBehaviour {
             } else {
                 snapPos.y = carToMove.transform.position.y;
             }
-        }
-        // Car is facing right/left and is moving on x-axis
-        else {
+        } else {
+            // Car is facing right/left and is moving on x-axis
             snapPos.y = carToMove.transform.position.y;
             // Check if the position is outside of the maximum allowed movement values of car
             if (snapPos.x > maxValAxis && carToMove.length != 0) {
@@ -149,58 +115,27 @@ public class CarMovement : MonoBehaviour {
         return snapPos;
     }
 
-    public void MoveCar() {
-        // If there is no car in the way check if we can move car
-        if (!CarInWay()) {
-            carToMove.transform.position = posToMoveCar;
-            posOfCar = posToMoveCar;
-        }
-    }
-
     public void EndMove() {
-        // If we have a carToMove, deactivate it and set variable to null
-        if (carToMove) {
-            if (posOfCar != startPosOfCar) {
-                if (undoList.Count > 0){
-                    if (undoList[undoList.Count - 1].car != carToMove) {
-                        gameManager.IncreaseMoves();
-                        AudioManager.Instance.PlayCarMoved();
-                        undoList.Add(new UndoTuple(carToMove, startPosOfCar));
-                        AbleToUndo();
-                    }
-                } else {
-                    gameManager.IncreaseMoves();
-                    AudioManager.Instance.PlayCarMoved();
-                    undoList.Add(new UndoTuple(carToMove, startPosOfCar));
-                    AbleToUndo();
-                }
+        if (carToMove && posOfCar != startPosOfCar) {
+            bool prevCar = false;
+            if (undoList.Count > 0) { prevCar = undoList[undoList.Count - 1].car != carToMove; }
+            if (!prevCar) {
+                GameManager.Instance.ChangeMoves(true);
+                AudioManager.Instance.PlayCarMoved();
+                undoList.Add(new UndoTuple(carToMove, startPosOfCar));
+                AbleToUndo();
             }
-            carToMove = null;
         }
+        carToMove = null;
     }
 
     // UNDO features
-    public void AbleToUndo() {
-        if (undoList.Count > 0) {
-            undoBtn.interactable = true;
-        } else {
-            undoBtn.interactable = false;
-        }
-    }
-
+    public void AbleToUndo() { undoBtn.interactable = undoList.Count > 0; }
     public void UndoMove() {
-        /* Undo the last move
-           "Move carToMove to posToMove" */
-            // Tuple (carToMove, posToMove)
-        gameManager.DecreaseMoves();
+        GameManager.Instance.ChangeMoves(false);
         int index = undoList.Count - 1;
-
-        carToMove = undoList[index].car;
-        posToMoveCar = undoList[index].movePos;
-        carToMove.transform.position = posToMoveCar;
-        
+        undoList[index].car.transform.position = undoList[index].movePos;
         undoList.RemoveAt(index);
-        carToMove = null;
         AbleToUndo();
     }
 }
