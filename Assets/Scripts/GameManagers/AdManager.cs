81451296c6345fc5742626b038846483679e4dcc
Halli <Halli.heima@gmail.com>
2020-12-31 15:42:42 +0000

    Update ad manager

--- Assets/Scripts/GameManagers/AdManager.cs
+++ Assets/Scripts/GameManagers/AdManager.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 using UnityEngine.Advertisements;
 
 public class AdManager : MonoBehaviour, IUnityAdsListener {
-
     public static AdManager Instance;
     #if UNITY_IOS
     string gameID = "3535681";
@@ -19,44 +16,38 @@ public class AdManager : MonoBehaviour, IUnityAdsListener {
         if (Instance == null) {
             Instance = this;
             Advertisement.AddListener(this);
-            Advertisement.Initialize(gameID, false);
+            Advertisement.Initialize(gameID);
             SceneManager.sceneLoaded += OnSceneLoaded;
+        } else {
+            Destroy(this);
         }
     }
-
     void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
-        if (scene.buildIndex == 0) {
-            rewardAdsBtn = GameObject.FindGameObjectsWithTag("rewardAdBtn");
-            foreach (GameObject btn in rewardAdsBtn){
-                btn.SetActive(false);
-            }
-        }
+        rewardAdsBtn = GameObject.FindGameObjectsWithTag("rewardAdBtn");
+        if (!Advertisement.IsReady()) { ToggleAdBtns(false); }
     }
 
     public void ShowVideoAd() {
         if (Advertisement.IsReady()) { Advertisement.Show(); }
     }
-
     public void ShowRewardVideo() {
-        Advertisement.Show(rewardVideoID);
+        if (Advertisement.IsReady()) { Advertisement.Show(rewardVideoID); }
     }
 
     public void OnUnityAdsReady(string placementId) {
-        if (placementId == rewardVideoID) {
-            foreach (GameObject btn in rewardAdsBtn) {
-                if (btn) { btn.SetActive(true); }
-            }
-        }
+        if (placementId == rewardVideoID) { ToggleAdBtns(true); }
     }
     public void OnUnityAdsDidStart(string placementId) {}
     public void OnUnityAdsDidError(string message) {}
     public void OnUnityAdsDidFinish(string placementId, ShowResult showResult) {
         if (placementId == rewardVideoID) {
             if (showResult == ShowResult.Finished) { GoldManager.Instance.AddGold(25, true); }
-
-            foreach (GameObject btn in rewardAdsBtn){
-                if (btn) { btn.SetActive(false); }
-            }
+            ToggleAdBtns(false);
+        }
+    }
+    void ToggleAdBtns(bool active) {
+        foreach (GameObject btn in rewardAdsBtn){
+            if (btn) { btn.SetActive(active); }
         }
     }
 }
