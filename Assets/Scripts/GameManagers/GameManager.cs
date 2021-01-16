b7747d5f675105fa834d1e3750c463199c2923d9
Haraldur-Dadi <halli.heima@gmail.com>
2021-01-16 17:56:41 +0000

    Update gamemanager
    Logic simplified

--- Assets/Scripts/GameManagers/GameManager.cs
+++ Assets/Scripts/GameManagers/GameManager.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour {
@@ -9,7 +8,6 @@ public class GameManager : MonoBehaviour {
     public int levelIndex;
     public int boardLength;
 
-    public GameObject gameBoard;
     public GameObject level_board;
     public GameObject[] level_boards_easy;
     public GameObject[] level_boards_medium;
@@ -23,59 +21,36 @@ public class GameManager : MonoBehaviour {
     void Awake() {
         if (Instance == null) {
             Instance = this;
+            if (CrossSceneManager.Instance.gameModeNr > 1) {
+                Camera.main.orthographicSize += 1.9f;
+            }
+            boardLength = level_boards_easy.Length;
+            LoadLevel();
         } else {
             Destroy(this);
         }
     }
-    void Start() {
-        if (CrossSceneManager.Instance.gameModeNr > 1) {
-            Camera.main.orthographicSize += 1.9f;
-        }
-        boardLength = level_boards_easy.Length;
-        LoadLevel();
-    }
 
     public virtual void LoadLevel() {
         GetComponent<CarMovement>().Refresh();
-        if (level_board != null)
-            Destroy(level_board);
         levelCompleteUi.SetActive(false);
 
         levelIndex = PlayerPrefs.GetInt("boardToLoad", 0);
-        GameObject boardToInst = null;
         if (levelIndex < boardLength) {
             difficultyTxt.text = "Easy";
-            boardToInst = level_boards_easy[levelIndex];
             CrossSceneManager.Instance.difficulty = 0;
-        } else if (boardLength <= levelIndex && levelIndex < (boardLength * 2)) {
+        } else if (levelIndex < (boardLength * 2)) {
             difficultyTxt.text = "Medium";
-            boardToInst = level_boards_medium[levelIndex - level_boards_easy.Length];
             CrossSceneManager.Instance.difficulty = 1;
-        } else if ((boardLength * 2) <= levelIndex && levelIndex < (boardLength * 3)) {
+        } else if (levelIndex < (boardLength * 3)) {
             difficultyTxt.text = "Hard";
-            boardToInst = level_boards_hard[levelIndex - (level_boards_easy.Length * 2)];
             CrossSceneManager.Instance.difficulty = 2;
-        } else if ((boardLength * 3) <= levelIndex) {
+        } else if (levelIndex >= (boardLength * 3)) {
             difficultyTxt.text = "Expert";
-            boardToInst = level_boards_expert[levelIndex - (level_boards_easy.Length * 3)];
             CrossSceneManager.Instance.difficulty = 3;
         }
-
-        level_board = Instantiate(boardToInst, gameBoard.transform);
-        ChangeCarSprites();
-    }
-
-    public void ChangeCarSprites() {
-        // Switches sprites of cars to match equipped
-        GameObject[] cars = GameObject.FindGameObjectsWithTag("Car");
-
-        foreach (GameObject car in cars) {
-            Car carObj = car.GetComponent<Car>();
-
-            if (carObj.length == 0) {
-                carObj.carImg.sprite = ItemDb.Instance.GetItem(PlayerPrefs.GetInt("PlayerCarEquipped", 0)).sprite;
-            }
-        }
+        level_board = Instantiate(level_boards_easy[levelIndex - (level_boards_easy.Length * CrossSceneManager.Instance.difficulty)], transform);
+        GameObject.FindWithTag("Car").GetComponent<SpriteRenderer>().sprite = ItemDb.Instance.GetItem(PlayerPrefs.GetInt("PlayerCarEquipped", 0)).sprite;
     }
 
     public virtual void ChangeMoves(bool increase) {}
@@ -111,6 +86,7 @@ public class GameManager : MonoBehaviour {
         
         CrossSceneManager.Instance.FadeBetweenObjects();
         yield return new WaitForSeconds(0.33f);
+        Destroy(level_board);
         LoadLevel();
     }
     public IEnumerator DelayedLevelSelector() {
