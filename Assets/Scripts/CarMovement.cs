544934546405f1cbec0a5422a866971b101bec83
Haraldur-Dadi <halli.heima@gmail.com>
2021-01-16 16:58:53 +0000

    Updated carmovment
    Now supports 8x8 & increased performance of car in way check

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CarMovement : MonoBehaviour {
-    public Vector2 startPosOfCar; // Vector positio, used for undo
+    public Vector2 startPosOfCar; // Vector position, used for undo
     public Vector2 posOfCar; // Vector position, used to determine current position of car
     public Vector2 posToMoveCar; // Vector position, used to determine where to move the car
 
     public Car carToMove;
+    public LayerMask mask;
     float snapValue = 1.1f;
 
     public Button undoBtn;
@@ -15,37 +16,29 @@ public class CarMovement : MonoBehaviour {
 
     public void Refresh() {
         undoList = new List<UndoTuple>();
-        AbleToUndo();
+        undoBtn.interactable = false;
     }
 
     bool CarInWay() {
-        /* Bool, checks if pathway to posToMoveCar is blocked by another car */
-        bool carInWay = false;
-        Vector2 raycastPos = posToMoveCar;
-
+        /* Checks if pathway to posToMoveCar is blocked by another car */
+        Vector3 rayPos = posToMoveCar;
         if (carToMove.transform.eulerAngles.z == 0 || carToMove.transform.eulerAngles.z == 180) {
-            raycastPos.y += (posToMoveCar.y > carToMove.transform.position.y) ? snapValue : -snapValue;
+            rayPos.y += (posToMoveCar.y > carToMove.transform.position.y) ? snapValue : -snapValue;
         } else {
-            raycastPos.x += (posToMoveCar.x > carToMove.transform.position.x) ? snapValue : -snapValue;
+            rayPos.x += (posToMoveCar.x > carToMove.transform.position.x) ? snapValue : -snapValue;
         }
-
-        RaycastHit2D[] hits = Physics2D.LinecastAll(raycastPos, carToMove.transform.position);
-        foreach (RaycastHit2D hit in hits) {
-            Car car = hit.transform.GetComponent<Car>();
-            if (car != carToMove && car != null) {
-                carInWay = true;
-            }
-        }
-        return carInWay;
+        return carToMove != Physics2D.Linecast(rayPos, carToMove.transform.position, mask).transform.GetComponent<Car>();
     }
 
-    Car RaycastAtPosition(Vector2 posToCheck) {
+    void RaycastAtPosition(Vector2 posToCheck) {
         /* Raycast at position to see if we hit Car object */
         RaycastHit2D hit = Physics2D.Raycast(posToCheck, Vector2.zero);
-        return hit.transform.GetComponent<Car>();
+        if (hit) {
+            carToMove = hit.transform.GetComponent<Car>();
+        }
     }
 
-    private void Update() {
+    void Update() {
         /* If touch on screen determine what to do */
         if (Input.touchCount > 0) {
             Touch touch = Input.GetTouch(0);
@@ -53,8 +46,10 @@ public class CarMovement : MonoBehaviour {
             switch (touch.phase) {
                 case TouchPhase.Began:
                     // Check if player touched a car
-                    carToMove = RaycastAtPosition(Camera.main.ScreenToWorldPoint(touch.position));
-                    if (carToMove) { startPosOfCar = posOfCar = carToMove.transform.position; }
+                    RaycastAtPosition(Camera.main.ScreenToWorldPoint(touch.position));
+                    if (carToMove != null) { 
+                        startPosOfCar = posOfCar = carToMove.transform.position;
+                    }
                     break;
                 case TouchPhase.Moved:
                     // If we have a carToMove, determine where to move it on board
@@ -68,10 +63,7 @@ public class CarMovement : MonoBehaviour {
                         }
                     }
                     break;
-                case TouchPhase.Canceled:
-                    EndMove();
-                    break;
-                case TouchPhase.Ended:
+                case TouchPhase.Canceled: case TouchPhase.Ended:
                     EndMove();
                     break;
             }
@@ -80,9 +72,12 @@ public class CarMovement : MonoBehaviour {
 
     public Vector2 SnapPosToGrid(Vector2 snapPos, float maxValAxis) {
         /* Create grid like movement on board by rounding and clamping to within the boards border */
+        maxValAxis += CrossSceneManager.Instance.gameModeNr > 1 ? 1.1f : 0f; 
+
         if (carToMove.transform.eulerAngles.z == 0 || carToMove.transform.eulerAngles.z == 180) {
             // Car is facing up/down and is moving on y-axis
             snapPos.x = carToMove.transform.position.x;
+
             // Check if the position is outside of the maximum allowed movement values of car
             if (snapPos.y > maxValAxis) {
                 snapPos.y = maxValAxis;
@@ -98,6 +93,7 @@ public class CarMovement : MonoBehaviour {
         } else {
             // Car is facing right/left and is moving on x-axis
             snapPos.y = carToMove.transform.position.y;
+            
             // Check if the position is outside of the maximum allowed movement values of car
             if (snapPos.x > maxValAxis && carToMove.length != 0) {
                 snapPos.x = maxValAxis;
@@ -118,25 +114,26 @@ public class CarMovement : MonoBehaviour {
     public void EndMove() {
         if (carToMove && posOfCar != startPosOfCar) {
             bool prevCar = false;
-            if (undoList.Count > 0) { prevCar = undoList[undoList.Count - 1].car != carToMove; }
+            if (undoList.Count > 0) { 
+                prevCar = (undoList[undoList.Count - 1].car == carToMove); 
+            }
             if (!prevCar) {
                 GameManager.Instance.ChangeMoves(true);
                 AudioManager.Instance.PlayCarMoved();
                 undoList.Add(new UndoTuple(carToMove, startPosOfCar));
-                AbleToUndo();
+                undoBtn.interactable = undoList.Count > 0;
             }
         }
         carToMove = null;
     }
 
-    public void AbleToUndo() { undoBtn.interactable = undoList.Count > 0; }
     public void UndoMove() {
         AudioManager.Instance.PlayButtonClick();
         GameManager.Instance.ChangeMoves(false);
         int index = undoList.Count - 1;
         undoList[index].car.transform.position = undoList[index].movePos;
         undoList.RemoveAt(index);
-        AbleToUndo();
+        undoBtn.interactable = undoList.Count > 0;
     }
 }
 
