1d58e8172d002de10238516501cc92e51d974a8b
Haraldur-Dadi <Halli.heima@gmail.com>
2021-03-05 13:08:52 +0000

    Updated challenge levels in level scene
    Slide before start of challenge level that says how many moves each star is

--- Assets/Scripts/GameManagers/Challenge_GM.cs
+++ Assets/Scripts/GameManagers/Challenge_GM.cs
@@ -9,6 +9,11 @@ public class Challenge_GM : GameManager {
     public string[] looseMessages;
     public GameObject nextLvlBtn;
 
+    public GameObject infoPanel;
+    public TextMeshProUGUI goldMoves;
+    public TextMeshProUGUI silverMoves;
+    public TextMeshProUGUI bronzeMoves;
+
     public Image star;
     public Sprite blankStar;
     public Sprite goldStar;
@@ -16,6 +21,7 @@ public class Challenge_GM : GameManager {
     public Sprite bronzeStar;
 
     public int moves;
+    private int currLevelMoves;
     public ChallengeLevelDb challengeLevelDb;
 
     public override void Awake() {
@@ -26,8 +32,18 @@ public class Challenge_GM : GameManager {
         }
     }
 
+    public void HideInfoPanel() {
+        infoPanel.SetActive(false);
+    }
+
     public override void LoadLevel() {
         base.LoadLevel();
+        infoPanel.SetActive(true);
+        currLevelMoves = challengeLevelDb.GetChallengeLevel(levelIndex);
+        goldMoves.text = currLevelMoves.ToString();
+        silverMoves.text = (currLevelMoves+1) + " - " + (currLevelMoves+3);
+        bronzeMoves.text = (currLevelMoves+4) + " - " + (currLevelMoves+7);
+
         moves = 0;
         movesTxt.text = "Moves: " + moves;
     }
@@ -40,44 +56,44 @@ public class Challenge_GM : GameManager {
     public override void LevelComplete() {
         levelCompleteUi.SetActive(true);
         PlayerPrefs.SetInt("LevelsCompleted", PlayerPrefs.GetInt("LevelsCompleted", 1) + 1);
+        CarMovement carMovement = GetComponent<CarMovement>();
+        if (carMovement.undoList.Count > 0) {
+            if (carMovement.carToMove != carMovement.undoList[carMovement.undoList.Count - 1].car) {
+                moves += 1;
+            }
+        }
+        completedMovesTxt.text = "Moves: " + (moves);
 
-        ChallengeLevel currLevel = challengeLevelDb.GetChallengeLevel(levelIndex);
-        if (moves < currLevel.minMoves + 6) {
+        if (moves <= currLevelMoves + 7) {
             completedTxt.text = winMessages[Random.Range(0, winMessages.Length)];
             string mode = (CrossSceneManager.Instance.gameModeNr == 2) ? "Challenge" : "8x8 Challenge";
             if (PlayerPrefs.GetInt(mode + "LevelReached", 0) <= levelIndex) {
                 PlayerPrefs.SetInt(mode + "LevelReached", levelIndex + 1);
             }
-            
+
             levelCompleteUi.GetComponent<Image>().color = new Color32(30,236,34,245);
-
-            CarMovement carMovement = GetComponent<CarMovement>();
-            if (carMovement.carToMove != carMovement.undoList[carMovement.undoList.Count - 1].car) {
-                moves += 1;
-            }
-
-            completedMovesTxt.text = "Moves: " + (moves);
-            if (moves <= currLevel.minMoves) {
+            if (moves <= currLevelMoves) {
                 star.sprite = goldStar;
                 movesNextStarTxt.text = "";
                 PlayerPrefs.SetInt(mode + levelIndex + "Stars", 3);
-            } else if (moves <= currLevel.minMoves + 2) {
+            } else if (moves <= currLevelMoves + 3) {
                 star.sprite = silverStar;
-                movesNextStarTxt.text = "Next star: " + currLevel.minMoves;
+                movesNextStarTxt.text = "Next star: " + currLevelMoves;
                 if (PlayerPrefs.GetInt(mode + levelIndex + "Stars") < 2)
                     PlayerPrefs.SetInt(mode + levelIndex + "Stars", 2);
             } else {
                 star.sprite = bronzeStar;
-                movesNextStarTxt.text = "Next star: " + (currLevel.minMoves + 2);
+                movesNextStarTxt.text = "Next star: " + (currLevelMoves + 3);
                 if (PlayerPrefs.GetInt(mode + levelIndex + "Stars") < 1)
                     PlayerPrefs.SetInt(mode + levelIndex + "Stars", 1);
             }
+            nextLvlBtn.SetActive(true);
         } else {
             completedTxt.text = looseMessages[Random.Range(0, looseMessages.Length)];
             levelCompleteUi.GetComponent<Image>().color = new Color32(236,51,30,245);
             star.sprite = blankStar;
-            movesNextStarTxt.text = "Minimum moves: " + (currLevel.minMoves + 6);
+            movesNextStarTxt.text = "Minimum moves: " + (currLevelMoves + 7);
+            nextLvlBtn.SetActive(false);
         }
-        nextLvlBtn.SetActive(moves < currLevel.minMoves + 6);
     }
 }
